8a6f0f8811dda96c0539b4c08e140bfa4482b422
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2025-04-19 16:34:16 +0200

    properly uses firebase secret

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 require('dotenv').config();
 
-// prettier-ignore
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || functions.config().discord.webhook_url;
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+
+if (!DISCORD_WEBHOOK_URL) {
+  throw new Error('DISCORD_WEBHOOK_URL is not set in environment variables');
+}
