44c75038ab262e1665f57fdf295a6ed722f37a08
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2025-04-19 16:14:03 +0200

    firebase functions, env option

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,5 @@
+const functions = require('firebase-functions');
 require('dotenv').config();
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+
+// prettier-ignore
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL || functions.config().discord.webhook_url;
